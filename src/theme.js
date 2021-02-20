b58f830e3d36df1b597df120e4b8952eef98270d
santiaago <santiago.arias@red-gate.com>
2021-02-20 14:52:44 +0000

    fix warning

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,22 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from "@material-ui/core/colors"
+// https://stackoverflow.com/a/64135466/989227
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from "@material-ui/core/styles"
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
-});
+})
 
-export default theme;
+export default theme
